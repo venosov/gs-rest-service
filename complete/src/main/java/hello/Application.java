41a5788d90f2f962cf975146e01f24f04902579e
venosov <venosov@gmail.com>
2017-04-29 23:06:07 +0200

    swagger

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,40 @@
 package hello;
-
+ 
+import static springfox.documentation.builders.PathSelectors.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+ 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
-
+ 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+     
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("greetings")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/greeting.*"))
+                .build();
+    }
+     
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/spring-guides/gs-rest-service/blob/master/LICENSE.code.txt")
+                .version("2.0")
+                .build();
     }
 }
